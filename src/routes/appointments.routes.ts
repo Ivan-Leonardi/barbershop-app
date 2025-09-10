c2cd092329fd093e1b9bab346954e33aa3563ca6
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2025-09-10 12:14:27 -0300

    feat: add appointmentsRoutes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,28 @@
 import { Router } from 'express'
+import { startOfHour, parseISO } from 'date-fns'
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
 const appointmentsRoutes = Router()
 
-appointmentsRoutes.get('/', (request, response) => {
-  return response.json({ message: 'List of appointments' })
+const appointmentsRepository = new AppointmentsRepository()
+
+appointmentsRoutes.post('/', (request, response) => {
+  const { provider, date } = request.body
+
+  const parsedDate = startOfHour(parseISO(date))
+
+  const findAppointmentInSameDate =
+    appointmentsRepository.findBydate(parsedDate)
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ error: 'This appointment is already booked' })
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate)
+
+  return response.json(appointment)
 })
 
 export default appointmentsRoutes
